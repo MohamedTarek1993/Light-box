8bd93fe7904a0b8ec4562df624c0b1fe18194e80
MohamedTarek1993 <www.mtarek789@gmail.com>
2021-10-21 14:45:29 +0200

    light box

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//============ Animate navbar ============
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  //Animate Links
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+
+  //Hamburger Animation
+  hamburger.classList.toggle("toggle");
+});
+//============ Animate navbar ============
+
+//=========== Active navbar Link in multi pages =============
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll(".nav-links li a");
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "active";
+  }
+}
+//=========== Active navbar Link in multi pages =============
